db980650dfd4dd1d90bdec32f3c57f7d33968e60
cfee8347 <cfee8347@student.pps.net>
2024-05-09 11:24:54 -0700

    done

---
+++ Card.java
@@ -0,0 +1,19 @@
+public class Card {
+
+    private int num;
+    private String suit;
+
+    public Card(int num, String suit) {
+        this.num = num;
+        this.suit = suit;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public String getSuit(){
+        return suit;
+    }
+
+}
---
+++ EuchreGame.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class EuchreGame {
+
+    public static void main(String[] args) {
+        addCards();
+        printDeck();
+    }
+
+    static ArrayList<Card> deck = new ArrayList<>();
+
+    public static void addCards(){
+        for (int x = 0; x < 4; x++){
+            for (int y = 1; y <= 13; y++){
+                if (x == 0){
+                    deck.add(new Card(y, "Club"));
+                } else if (x == 1){
+                    deck.add(new Card(y, "Diamond"));
+                } else if (x == 2){
+                    deck.add(new Card(y, "Spade"));
+                } else if (x == 3){
+                    deck.add(new Card(y, "Heart"));
+                }
+            }
+        }
+    }
+
+    public static void printDeck(){
+        for (int x = 0; x < deck.size(); x++){
+            if (deck.get(x).getNum() == 11){
+                System.out.println("Jack of " + deck.get(x).getSuit() + "s");
+            } else if (deck.get(x).getNum() == 12){
+                System.out.println("Queen of " + deck.get(x).getSuit() + "s");
+            } else if (deck.get(x).getNum() == 13){
+                System.out.println("King of " + deck.get(x).getSuit() + "s");
+            } else {
+                System.out.println(deck.get(x).getNum() + " of " + deck.get(x).getSuit() + "s");
+            }
+        }
+    }
+
+}
