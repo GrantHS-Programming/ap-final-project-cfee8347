43677fcf52aac96ef88add9def293babceecfcbd
cfee8347 <cfee8347@student.pps.net>
2024-06-05 11:37:32 -0700

    done

--- EuchreGame.java
+++ EuchreGame.java
@@ -28,55 +28,55 @@ public class EuchreGame {
     static Player player = new Player("player");
     static ArrayList<Card> currentTrick = new ArrayList<>();
     static Team team1 = new Team(computer1, computer3);
-    static Team team2 = new Team (computer2, player);
+    static Team team2 = new Team(computer2, player);
 
-    public static void addCards(){
-        if (!deck.isEmpty()){
+    public static void addCards() {
+        if (!deck.isEmpty()) {
             deck.subList(0, deck.size()).clear();
         }
-        for (int x = 0; x < 4; x++){
-            for (int y = 9; y <= 14; y++){
-                if (x == 0){
+        for (int x = 0; x < 4; x++) {
+            for (int y = 9; y <= 14; y++) {
+                if (x == 0) {
                     deck.add(new Card(Integer.toString(y), "Club"));
-                } else if (x == 1){
+                } else if (x == 1) {
                     deck.add(new Card(Integer.toString(y), "Diamond"));
-                } else if (x == 2){
+                } else if (x == 2) {
                     deck.add(new Card(Integer.toString(y), "Spade"));
-                } else if (x == 3){
+                } else if (x == 3) {
                     deck.add(new Card(Integer.toString(y), "Heart"));
                 }
             }
         }
     }
 
-    public static void printDeck(){
+    public static void printDeck() {
         for (Card card : deck) {
             System.out.print(card.getCard() + ", ");
         }
         System.out.println("");
     }
 
-    public static void gameSetup(){
+    public static void gameSetup() {
         int dealer = (int) (Math.random() * 4) + 1;
-        if (dealer == 1){
+        if (dealer == 1) {
             computer2.setLead(true);
             computer1.setSeat(0);
             computer2.setSeat(1);
             computer3.setSeat(2);
             player.setSeat(3);
-        } else if (dealer == 2){
+        } else if (dealer == 2) {
             computer3.setLead(true);
             computer1.setSeat(3);
             computer2.setSeat(0);
             computer3.setSeat(1);
             player.setSeat(2);
-        } else if (dealer == 3){
+        } else if (dealer == 3) {
             player.setLead(true);
             computer1.setSeat(2);
             computer2.setSeat(3);
             computer3.setSeat(0);
             player.setSeat(1);
-        } else if (dealer == 4){
+        } else if (dealer == 4) {
             computer1.setLead(true);
             computer1.setSeat(1);
             computer2.setSeat(2);
@@ -87,114 +87,174 @@ public class EuchreGame {
 
         for (int y = 1; y <= 4; y++) {
             for (int x = 0; x < 5; x++) {
-                if (y == 1){
+                if (y == 1) {
                     computer1.addCardToHand(deck.remove((int) (Math.random() * deck.size())));
-                } else if (y == 2){
+                } else if (y == 2) {
                     computer2.addCardToHand(deck.remove((int) (Math.random() * deck.size())));
-                } else if (y== 3){
+                } else if (y == 3) {
                     computer3.addCardToHand(deck.remove((int) (Math.random() * deck.size())));
-                } else if (y == 4){
+                } else if (y == 4) {
                     player.addCardToHand(deck.remove((int) (Math.random() * deck.size())));
                 }
             }
         }
         trumpCard = deck.get(deck.size() - 1);
 
-        for (Card card : player.getHand()){
-            if (card.getSuit().equals(trumpCard.getSuit()) && card.getActualNum() == 11){
+        for (Card card : player.getHand()) {
+            if (card.getSuit().equals(trumpCard.getSuit()) && card.getActualNum() == 11) {
                 card.setRight(true);
                 System.out.println(card.getNum() + " " + card.getSuit());
-            }else if (trumpCard.getSuit().equals("Club") && card.getSuit().equals("Spade") && card.getActualNum() == 11){
+            } else if (trumpCard.getSuit().equals("Club") && card.getSuit().equals("Spade") && card.getActualNum() == 11) {
                 card.setLeft(true);
-            } else if (trumpCard.getSuit().equals("Diamond") && card.getSuit().equals("Heart") && card.getActualNum() == 11){
+            } else if (trumpCard.getSuit().equals("Diamond") && card.getSuit().equals("Heart") && card.getActualNum() == 11) {
                 card.setLeft(true);
-            } else if (trumpCard.getSuit().equals("Spade") && card.getSuit().equals("Club") && card.getActualNum() == 11){
+            } else if (trumpCard.getSuit().equals("Spade") && card.getSuit().equals("Club") && card.getActualNum() == 11) {
                 card.setLeft(true);
-            } else if (trumpCard.getSuit().equals("Heart") && card.getSuit().equals("Diamond") && card.getActualNum() == 11){
+            } else if (trumpCard.getSuit().equals("Heart") && card.getSuit().equals("Diamond") && card.getActualNum() == 11) {
                 card.setLeft(true);
             }
         }
-        for (Card card : computer1.getHand()){
-            if (card.getSuit().equals(trumpCard.getSuit()) && card.getActualNum() == 11){
+        for (Card card : computer1.getHand()) {
+            if (card.getSuit().equals(trumpCard.getSuit()) && card.getActualNum() == 11) {
                 card.setRight(true);
                 System.out.println(card.getNum() + " " + card.getSuit());
-            }else if (trumpCard.getSuit().equals("Club") && card.getSuit().equals("Spade") && card.getActualNum() == 11){
+            } else if (trumpCard.getSuit().equals("Club") && card.getSuit().equals("Spade") && card.getActualNum() == 11) {
                 card.setLeft(true);
-            } else if (trumpCard.getSuit().equals("Diamond") && card.getSuit().equals("Heart") && card.getActualNum() == 11){
+            } else if (trumpCard.getSuit().equals("Diamond") && card.getSuit().equals("Heart") && card.getActualNum() == 11) {
                 card.setLeft(true);
-            } else if (trumpCard.getSuit().equals("Spade") && card.getSuit().equals("Club") && card.getActualNum() == 11){
+            } else if (trumpCard.getSuit().equals("Spade") && card.getSuit().equals("Club") && card.getActualNum() == 11) {
                 card.setLeft(true);
-            } else if (trumpCard.getSuit().equals("Heart") && card.getSuit().equals("Diamond") && card.getActualNum() == 11){
+            } else if (trumpCard.getSuit().equals("Heart") && card.getSuit().equals("Diamond") && card.getActualNum() == 11) {
                 card.setLeft(true);
             }
         }
-        for (Card card : computer2.getHand()){
-            if (card.getSuit().equals(trumpCard.getSuit()) && card.getActualNum() == 11){
+        for (Card card : computer2.getHand()) {
+            if (card.getSuit().equals(trumpCard.getSuit()) && card.getActualNum() == 11) {
                 card.setRight(true);
                 System.out.println(card.getNum() + " " + card.getSuit());
-            }else if (trumpCard.getSuit().equals("Club") && card.getSuit().equals("Spade") && card.getActualNum() == 11){
+            } else if (trumpCard.getSuit().equals("Club") && card.getSuit().equals("Spade") && card.getActualNum() == 11) {
                 card.setLeft(true);
-            } else if (trumpCard.getSuit().equals("Diamond") && card.getSuit().equals("Heart") && card.getActualNum() == 11){
+            } else if (trumpCard.getSuit().equals("Diamond") && card.getSuit().equals("Heart") && card.getActualNum() == 11) {
                 card.setLeft(true);
-            } else if (trumpCard.getSuit().equals("Spade") && card.getSuit().equals("Club") && card.getActualNum() == 11){
+            } else if (trumpCard.getSuit().equals("Spade") && card.getSuit().equals("Club") && card.getActualNum() == 11) {
                 card.setLeft(true);
-            } else if (trumpCard.getSuit().equals("Heart") && card.getSuit().equals("Diamond") && card.getActualNum() == 11){
+            } else if (trumpCard.getSuit().equals("Heart") && card.getSuit().equals("Diamond") && card.getActualNum() == 11) {
                 card.setLeft(true);
             }
         }
-        for (Card card : computer3.getHand()){
-            if (card.getSuit().equals(trumpCard.getSuit()) && card.getActualNum() == 11){
+        for (Card card : computer3.getHand()) {
+            if (card.getSuit().equals(trumpCard.getSuit()) && card.getActualNum() == 11) {
                 card.setRight(true);
                 System.out.println(card.getNum() + " " + card.getSuit());
-            }else if (trumpCard.getSuit().equals("Club") && card.getSuit().equals("Spade") && card.getActualNum() == 11){
+            } else if (trumpCard.getSuit().equals("Club") && card.getSuit().equals("Spade") && card.getActualNum() == 11) {
                 card.setLeft(true);
-            } else if (trumpCard.getSuit().equals("Diamond") && card.getSuit().equals("Heart") && card.getActualNum() == 11){
+            } else if (trumpCard.getSuit().equals("Diamond") && card.getSuit().equals("Heart") && card.getActualNum() == 11) {
                 card.setLeft(true);
-            } else if (trumpCard.getSuit().equals("Spade") && card.getSuit().equals("Club") && card.getActualNum() == 11){
+            } else if (trumpCard.getSuit().equals("Spade") && card.getSuit().equals("Club") && card.getActualNum() == 11) {
                 card.setLeft(true);
-            } else if (trumpCard.getSuit().equals("Heart") && card.getSuit().equals("Diamond") && card.getActualNum() == 11){
+            } else if (trumpCard.getSuit().equals("Heart") && card.getSuit().equals("Diamond") && card.getActualNum() == 11) {
                 card.setLeft(true);
             }
         }
-        for (Card card : deck){
-            if (card.getSuit().equals(trumpCard.getSuit()) && card.getActualNum() == 11){
+        for (Card card : deck) {
+            if (card.getSuit().equals(trumpCard.getSuit()) && card.getActualNum() == 11) {
                 card.setRight(true);
                 System.out.println(card.getNum() + " " + card.getSuit());
-            }else if (trumpCard.getSuit().equals("Club") && card.getSuit().equals("Spade") && card.getActualNum() == 11){
+            } else if (trumpCard.getSuit().equals("Club") && card.getSuit().equals("Spade") && card.getActualNum() == 11) {
                 card.setLeft(true);
-            } else if (trumpCard.getSuit().equals("Diamond") && card.getSuit().equals("Heart") && card.getActualNum() == 11){
+            } else if (trumpCard.getSuit().equals("Diamond") && card.getSuit().equals("Heart") && card.getActualNum() == 11) {
                 card.setLeft(true);
-            } else if (trumpCard.getSuit().equals("Spade") && card.getSuit().equals("Club") && card.getActualNum() == 11){
+            } else if (trumpCard.getSuit().equals("Spade") && card.getSuit().equals("Club") && card.getActualNum() == 11) {
                 card.setLeft(true);
-            } else if (trumpCard.getSuit().equals("Heart") && card.getSuit().equals("Diamond") && card.getActualNum() == 11){
+            } else if (trumpCard.getSuit().equals("Heart") && card.getSuit().equals("Diamond") && card.getActualNum() == 11) {
                 card.setLeft(true);
             }
         }
     }
 
-    public static boolean isTeamTaking(){
-        if (currentTrick.size() == 2){
-            if (currentTrick.get(1).getSuit().equals(currentTrick.get(0).getSuit()) && currentTrick.get(0).getActualNum() > currentTrick.get(1).getActualNum()){
-                return true;
-            } else if (currentTrick.get(1).getSuit().equals(currentTrick.get(0).getSuit()) && currentTrick.get(0).getActualNum() < currentTrick.get(1).getActualNum()){
+    public static boolean isTeamTaking() {
+        System.out.println("1");
+        if (currentTrick.size() == 2) {
+            System.out.println("2");
+            if (currentTrick.get(1).isRight()){
+                System.out.println("18");
                 return false;
-            } else if (currentTrick.get(1).getSuit().equals(trumpCard.getSuit()) && !currentTrick.get(0).getSuit().equals(trumpCard.getSuit())){
-                return false;
-            } else if (!currentTrick.get(0).getSuit().equals(currentTrick.get(1).getSuit()) && !currentTrick.get(1).getSuit().equals(trumpCard.getSuit())){
+            } else if (currentTrick.get(0).isRight()){
+                System.out.println("19");
                 return true;
+            } else if (currentTrick.get(0).isLeft()){
+                System.out.println("20");
+                return true;
+            }  else if (currentTrick.get(1).isLeft()){
+                System.out.println("21");
+                return false;
+            } else if (currentTrick.get(1).getSuit().equals(currentTrick.get(0).getSuit()) && currentTrick.get(0).getActualNum() > currentTrick.get(1).getActualNum()) {
+                System.out.println("3");
+                return true;
+            } else if (currentTrick.get(1).getSuit().equals(currentTrick.get(0).getSuit()) && currentTrick.get(0).getActualNum() < currentTrick.get(1).getActualNum()) {
+                System.out.println("4");
+                return false;
+            } else if (currentTrick.get(1).getSuit().equals(trumpCard.getSuit()) && !currentTrick.get(0).getSuit().equals(trumpCard.getSuit())) {
+                System.out.println("5");
+                return false;
+            } else if (!currentTrick.get(0).getSuit().equals(currentTrick.get(1).getSuit()) && !currentTrick.get(1).getSuit().equals(trumpCard.getSuit())) {
+                System.out.println("6");
+                return true;
+            } else {
+                System.out.println("7");
+                return false;
             }
-        } else if (currentTrick.size() == 2){
-            if (currentTrick.get(0).getSuit().equals(currentTrick.get(1).getSuit()) && currentTrick.get(0).getSuit().equals(currentTrick.get(2).getSuit()) && currentTrick.get(1).getActualNum() < currentTrick.get(0).getActualNum() && currentTrick.get(1).getActualNum() < currentTrick.get(2).getActualNum()){
-                return true;
-            } else if (currentTrick.get(0).getSuit().equals(currentTrick.get(1).getSuit()) && currentTrick.get(0).getSuit().equals(currentTrick.get(2).getSuit()) && currentTrick.get(1).getActualNum() > currentTrick.get(0).getActualNum() || currentTrick.get(1).getActualNum() > currentTrick.get(2).getActualNum()){
+        } else if (currentTrick.size() > 2) {
+            System.out.println("8");
+            if (currentTrick.get(0).isRight() || currentTrick.get(2).isRight()){
+                System.out.println("17");
                 return false;
-            } else if (currentTrick.get(1).getSuit().equals(trumpCard.getSuit()) && !currentTrick.get(0).getSuit().equals(trumpCard.getSuit()) && !currentTrick.get(2).getSuit().equals(trumpCard.getSuit())){
+            } else if (currentTrick.get(1).isRight()){
+                System.out.println("22");
                 return true;
-            } else if (currentTrick.get(0).getSuit().equals(trumpCard.getSuit()) || currentTrick.get(2).getSuit().equals(trumpCard.getSuit()) && !currentTrick.get(1).getSuit().equals(trumpCard.getSuit())){
+            } else if (currentTrick.get(0).isLeft() || currentTrick.get(2).isLeft()){
+                System.out.println("23");
+                return false;
+            } else if (currentTrick.get(1).isLeft()){
+                System.out.println("24");
+                return true;
+            } else if (currentTrick.get(1).getSuit().equals(currentTrick.get(0).getSuit()) && currentTrick.get(2).getSuit().equals(currentTrick.get(0).getSuit())){
+                System.out.println("25");
+                if (currentTrick.get(1).getActualNum() < currentTrick.get(0).getActualNum() || currentTrick.get(1).getActualNum() < currentTrick.get(2).getActualNum()) {
+                    System.out.println("10");
+                    return false;
+                } else {
+                    System.out.println("16");
+                    return true;
+                }
+            } else if (currentTrick.get(1).getSuit().equals(currentTrick.get(0).getSuit()) && !currentTrick.get(2).getSuit().equals(currentTrick.get(0).getSuit())){
+                System.out.println(26);
+                if (currentTrick.get(1).getActualNum() < currentTrick.get(0).getActualNum()){
+                    System.out.println("10");
+                    return false;
+                } else {
+                    System.out.println("16");
+                    return true;
+                }
+            } else if (currentTrick.get(1).getSuit().equals(trumpCard.getSuit()) && !currentTrick.get(0).getSuit().equals(trumpCard.getSuit()) && !currentTrick.get(2).getSuit().equals(trumpCard.getSuit())) {
+                System.out.println("11");
+                return true;
+            } else if (currentTrick.get(0).getSuit().equals(trumpCard.getSuit()) || currentTrick.get(2).getSuit().equals(trumpCard.getSuit()) && !currentTrick.get(1).getSuit().equals(trumpCard.getSuit())) {
+                System.out.println("12");
+                return false;
+            } else if (!currentTrick.get(1).getSuit().equals(currentTrick.get(0).getSuit()) && !currentTrick.get(1).getSuit().equals(trumpCard.getSuit())) {
+                System.out.println("15");
+                return false;
+            } else {
+                System.out.println("13");
                 return false;
             }
+        } else {
+            System.out.println("14");
+            return false;
         }
     }
+
 
 
     public static void playATrick(){
@@ -379,7 +439,6 @@ public class EuchreGame {
                 if (currentTrick.get(x).isRight()){
                     rightOrLeft = true;
                     whoTook = x;
-                    System.out.println(x + " took with right");
                     break;
                 }
             }
@@ -492,6 +551,7 @@ public class EuchreGame {
 
     public static void playerPlayACard(){
         while (true) {
+            System.out.println("Your team is taking: " + isTeamTaking());
             System.out.println("What card would you like to play?");
             int cardIndex = input.nextInt();
             if (currentTrick.isEmpty()) {
